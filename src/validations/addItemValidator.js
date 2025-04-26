a69cbb2a5f0568928470a97a34a2efbf54aa677c
Jawad-Jatri <jawad.jatri@gmail.com>
2025-04-26 20:46:21 +0600

    fix(cartItem): add unitPrice validation in validator

--- src/validations/addItemValidator.js
+++ src/validations/addItemValidator.js
@@ -2,7 +2,8 @@ const Joi = require('joi');
 
 const addItemValidator = Joi.object({
     serviceName: Joi.string().required(),
-    quantity: Joi.number().required()
+    quantity: Joi.number().required(),
+    unitPrice: Joi.number().required()
 });
 
 module.exports = addItemValidator;
